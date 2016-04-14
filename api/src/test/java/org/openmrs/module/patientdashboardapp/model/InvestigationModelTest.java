f2ca5c54be22021b1a7c135f9eb8e2e5b14f69f9
geoff-wasilwa <geoffreywanjala@gmail.com>
2016-04-14 17:18:49 +0300

    Feat skip zero amount investigations

--- api/src/test/java/org/openmrs/module/patientdashboardapp/model/InvestigationModelTest.java
+++ api/src/test/java/org/openmrs/module/patientdashboardapp/model/InvestigationModelTest.java
@@ -1,7 +1,10 @@
 package org.openmrs.module.patientdashboardapp.model;
 
+import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openmrs.Encounter;
+import org.openmrs.api.context.Context;
 import org.openmrs.module.patientdashboardapp.model.Investigation;
 
 /**
@@ -14,5 +17,16 @@ public class InvestigationModelTest  extends AbstractModelTest {
         Encounter encounter = createEncounter();
         Investigation investigation = new Investigation(9996, "Maralia Tests");
         investigation.save(encounter,"My Department Test");
+    }
+    
+    @Test
+    @SuppressWarnings("deprecation")
+    public void save_shouldCreateOrderForFreeInvestigation() throws Exception {
+        executeDataSet("notes-concepts.xml");
+        Encounter encounter = createEncounter();
+        Investigation investigation = new Investigation(9996, "Maralia Tests");
+        investigation.save(encounter,"My Department Test");
+        
+        Assert.assertEquals(1, Context.getOrderService().getOrdersByPatient(encounter.getPatient()).size());
     }
 }
