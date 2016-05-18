2985c579e925b1a389f6f5d4dc03891d58ee86e5
kimani-njoroge <kimani064@gmail.com>
2016-05-18 12:54:50 +0300

    triagesummaries

---
+++ api/src/main/java/org/openmrs/module/patientdashboardapp/api/db/hibernate/HibernateTriageDAO.java
@@ -0,0 +1,30 @@
+package org.openmrs.module.patientdashboardapp.api.db.hibernate;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.openmrs.Patient;
+import org.openmrs.module.hospitalcore.model.TriagePatientData;
+import org.openmrs.module.patientdashboardapp.api.db.TriageDAO;
+
+public class HibernateTriageDAO implements TriageDAO {
+
+	@Override
+	public List<TriagePatientData> getPatientTriageData(Patient patient) {
+		Criteria criteria = sessionFactory.getCurrentSession().createCriteria(TriagePatientData.class);
+		criteria.add(Restrictions.eq("patient", patient));
+		return criteria.list();
+	}
+	public TriagePatientData getPatientTriageData(Integer id) {
+		Criteria criteria = sessionFactory.getCurrentSession().createCriteria(TriagePatientData.class);
+		criteria.add(Restrictions.eq("id", id));
+		return (TriagePatientData) criteria.list().get(0);
+	}
+	private SessionFactory sessionFactory;
+   
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+}
--- omod/src/main/java/org/openmrs/module/patientdashboardapp/fragment/controller/TriageInfoFragmentController.java
+++ omod/src/main/java/org/openmrs/module/patientdashboardapp/fragment/controller/TriageInfoFragmentController.java
@@ -1,15 +1,24 @@
 package org.openmrs.module.patientdashboardapp.fragment.controller;
 
+import java.util.List;
+
+import org.openmrs.Patient;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.hospitalcore.PatientQueueService;
 import org.openmrs.module.hospitalcore.model.OpdPatientQueue;
+import org.openmrs.module.hospitalcore.model.TriagePatientData;
+import org.openmrs.module.patientdashboardapp.api.TriageService;
+import org.openmrs.ui.framework.SimpleObject;
+import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.ui.framework.fragment.FragmentConfiguration;
 import org.openmrs.ui.framework.fragment.FragmentModel;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public class TriageInfoFragmentController {
 	public void controller(
 			FragmentConfiguration config,
-			FragmentModel model
+			FragmentModel model,
+			UiUtils uiutils
 			) {
 		config.require("patientId");
 		config.require("queueId");
@@ -17,8 +26,24 @@ public class TriageInfoFragmentController {
 		
 		Integer  patientId = Integer.valueOf(config.get("patientId").toString());
 		Integer queueId = Integer.valueOf(config.get("queueId").toString());
+		
+		Patient p = Context.getPatientService().getPatient(patientId);
+		
 		OpdPatientQueue opdPatientQueue = Context.getService(PatientQueueService.class).getOpdPatientQueueById(queueId);
+			
+		model.addAttribute("triageInfo", getTriageId(p, uiutils));
 		model.addAttribute("triage", opdPatientQueue.getTriageDataId());
 		model.addAttribute("patient", Context.getPatientService().getPatient(patientId));
+	}
+	
+	public List <SimpleObject> getTriageId(
+			@RequestParam("patientId") Patient patientId,UiUtils ui){
+		List<TriagePatientData> triage = Context.getService(TriageService.class).getPatientTriageData(patientId);
+		return  SimpleObject.fromCollection(triage, ui, "id" , "createdOn");
+	}	
+	public SimpleObject getTriageSummary(
+			@RequestParam("Id")Integer Id ,UiUtils ui){
+		TriagePatientData triad = Context.getService(TriageService.class).getPatientTriageData(Id);
+		return SimpleObject.fromObject(triad, ui, "temperature" ,"weight" ,"height" ,"BMI" ,"mua" ,"chest" ,"abdominal", "systolic" ,"daistolic", "respiratoryRate" ,"pulsRate" ,"bloodGroup","lastMenstrualDate", "rhesusFactor","pitct", "createdOn", "encounterOpd","oxygenSaturation");
 	}
 }
